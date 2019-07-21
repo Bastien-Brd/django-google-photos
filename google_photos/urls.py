2d2787472bbd941d2f42910ab9d250f2f0f5baf0
Bastien Bourdon <bb@edgefolio.com>
2019-07-21 17:18:55 +0100

    Add a dashboard page separate from the home page, for logged in users

--- google_photos/urls.py
+++ google_photos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
+
+from .views import HomeView, DashboardView
+
 
 app_name = "google_photos"
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="home.html")),
+    path('', HomeView.as_view(), name='home'),
+    path('dashboard', DashboardView.as_view(), name='dashboard'),
 ]
--- google_photos/views.py
+++ google_photos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import redirect, reverse
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_anonymous:
+            return redirect(reverse('google_photos:dashboard'))
+        return super(HomeView, self).dispatch(request, *args, **kwargs)
+
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
